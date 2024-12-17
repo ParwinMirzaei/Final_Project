e7164cb3ce8e8c511ccb594a8d96f241b1c8efdc
ParwinMirzaei <parwinmirzaei@yahoo.com>
2024-12-17 15:32:35 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,18 @@
 import React from 'react';
 import Sidebar from './Sidebar.jsx';
 import SidebarIcons from './SidebarIcons';
+import Feed from './Feed';
+import TweetBox from './TweetBox.jsx';
+import Widgets from './Widgets.jsx';
 
 export const App = () => {
   return (
-    <div className='App'>
+    <div className='app'>
       <Sidebar />
+      {/* Feed Section */}
+      <Feed />
+      {/* Widget Section */}
+      <Widgets />
     </div>
   );
 };
