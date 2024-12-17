b1ee00140a17b2930265512686d94d5f5ba5b86a
ParwinMirzaei <parwinmirzaei@yahoo.com>
2024-12-17 16:22:55 +0100

    Update TweetBox.jsx

--- src/TweetBox.jsx
+++ src/TweetBox.jsx
@@ -1,29 +1,47 @@
-import { Avatar, Button } from '@mui/material'
-import React from 'react';
-import "./TweetBox.css";
+import React, { useState } from 'react';
+import { Avatar, Button } from '@mui/material';
+import './TweetBox.css';
 
+function TweetBox({ onTweet }) {
+  const [content, setContent] = useState('');
+  const [image, setImage] = useState('');
 
-function TweetBox() {
-  
+  const handlePost = (e) => {
+    e.preventDefault();
+    if (content.trim()) {
+      onTweet(content, image);
+      setContent('');
+      setImage('');
+    }
+  };
+
   return (
-    <div className='tweetBox'>
-      <form>
-        <div className='tweetBox__input'>
+    <div className="tweetBox">
+      <form onSubmit={handlePost}>
+        <div className="tweetBox__input">
           <Avatar
-            src ="https://foerdern.koelnerzoo.de/images/patenschaften/tiere/flamingo/flamingo-2_1200x900.jpg" />
-            <input
-              type = "text"
-              placeholder="What's happening" />
+            src="https://foerdern.koelnerzoo.de/images/patenschaften/tiere/flamingo/flamingo-2_1200x900.jpg"
+          />
+          <input
+            type="text"
+            placeholder="What's happening?"
+            value={content}
+            onChange={(e) => setContent(e.target.value)}
+          />
         </div>
-            <input
-              className='tweetBox__imageInput'
-              placeholder='Image'
-              />
-        <Button
-          className='tweetBox__postButton'>Post</Button>
+        <input
+          className="tweetBox__imageInput"
+          type="text"
+          placeholder="Optional: Enter image URL"
+          value={image}
+          onChange={(e) => setImage(e.target.value)}
+        />
+        <Button type="submit" className="tweetBox__postButton">
+          Post
+        </Button>
       </form>
     </div>
-  )
+  );
 }
 
-export default TweetBox
+export default TweetBox;
